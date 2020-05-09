dfe0654dd25d3167144332d628804e3acff25fc5
shenshing <lyshenshing20@gmail.com>
2020-05-09 01:43:20 -0400

    first commit

---
+++ public/precache-manifest.770493020b7a3b915ca46cc161dbd966.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf866e89f06ce4396bd3e15368dd8c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "635e698c4c8aa2259bce",
+    "url": "/static/css/main.41a75575.chunk.css"
+  },
+  {
+    "revision": "b80ec6d415d590c36ac5",
+    "url": "/static/js/2.b4d43b89.chunk.js"
+  },
+  {
+    "revision": "4d3d17ca1d72509d468eda11f7e92a3e",
+    "url": "/static/js/2.b4d43b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635e698c4c8aa2259bce",
+    "url": "/static/js/main.bddce4e5.chunk.js"
+  },
+  {
+    "revision": "24b486c66c3a7276b55f",
+    "url": "/static/js/runtime-main.246e8137.js"
+  },
+  {
+    "revision": "876ab531f4684e22d33640159bc74748",
+    "url": "/static/media/background3.876ab531.jpeg"
+  }
+]);
