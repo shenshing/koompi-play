49029f156084fc4ce385122b47729e6ed74d443a
shenshing <lyshenshing20@gmail.com>
2020-06-12 01:42:36 -0400

    first done

---
+++ public/precache-manifest.ede0879e2e3b63e7d3566819b67fe2ff.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d117c460408c5bda2523be67f6482236",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd15aaea83abcf0ac2bd",
+    "url": "/static/css/2.857f90b2.chunk.css"
+  },
+  {
+    "revision": "c5f4cbfda7d10003bcd0",
+    "url": "/static/css/main.deb3cf95.chunk.css"
+  },
+  {
+    "revision": "cd15aaea83abcf0ac2bd",
+    "url": "/static/js/2.c4098df6.chunk.js"
+  },
+  {
+    "revision": "e7a31f68a4360bf9bc1675418fde5c3e",
+    "url": "/static/js/2.c4098df6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5f4cbfda7d10003bcd0",
+    "url": "/static/js/main.a54797fd.chunk.js"
+  },
+  {
+    "revision": "24b486c66c3a7276b55f",
+    "url": "/static/js/runtime-main.246e8137.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "876ab531f4684e22d33640159bc74748",
+    "url": "/static/media/background3.876ab531.jpeg"
+  },
+  {
+    "revision": "ca49bba4ee5bc19ee6dc0545e112b3e8",
+    "url": "/static/media/bars.ca49bba4.svg"
+  },
+  {
+    "revision": "3219419957b7d03578da90e20d8a7401",
+    "url": "/static/media/button-sound.32194199.mp3"
+  },
+  {
+    "revision": "37426e5d70c6ee8934bd3e563a8dba80",
+    "url": "/static/media/right-answer2.37426e5d.mp3"
+  },
+  {
+    "revision": "66615ba47ca38e6a4329077f240205ee",
+    "url": "/static/media/wrong-answer2.66615ba4.mp3"
+  }
+]);
